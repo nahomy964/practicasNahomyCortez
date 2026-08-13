0d9c4648004ebbea1ed7b9350bf1b6b414d66144
nahomy964 <nahomy.cortez2027@frech.superate.org>
2026-08-13 09:00:59 -0600

    practica java

--- practica0A/js/escript.js
+++ practica0A/js/escript.js
@@ -1,7 +1,9 @@
 
 
-function revisar(){
+function revisar(numero){
     let caja = document.getElementById("mensaje");
     //obtenemos el parrafo donde irá el mensaje
     caja.innerHTML="respuesta correcta!"
+
+    fuctio
 }
