9207fb7d85063b2b3529c44eeca91fcd2768b67c
nahomy964 <nahomy.cortez2027@frech.superate.org>
2026-08-17 10:38:12 -0600

    practica1js

---
+++ practica1js/js/script.js
@@ -0,0 +1,26 @@
+
+
+document.write("Hola mundo")
+
+alert("Mundo Hola")
+
+console.log("hola chavales")
+
+//tipos de datos
+let nombre= "Ana";
+
+let edad= 18;
+
+let estudiante = true;
+
+let ciudad;
+
+let mascota = null;
+
+//usando typeof para identificar cada tipo
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof estudiante);
+console.log(typeof ciudad);
+console.log(typeof mascota);
+
